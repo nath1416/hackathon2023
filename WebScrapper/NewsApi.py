70efc1d2665000b3c051989b39076689f5f95260
Nathan Bougie <202046701@edu.clg.qc.ca>
2023-02-04 23:28:43 -0500

    Progress Web Scrapper

--- WebScrapper/NewsApi.py
+++ WebScrapper/NewsApi.py
@@ -3,22 +3,24 @@ import json
 
 
 API_KEY = '5f74ffb5db5443ccb5c7910cb57095e2'
+DIRECTORY_DATABASE = "./database/NewsApi/"
+
+def filter_strings(body_content:str):
+    return ''.join(list(filter(lambda x: x.isalpha() or x.isnumeric() or x == " ", body_content)))
 
 def init_newsapi()->NewsApiClient:
     newsapi = NewsApiClient(api_key=API_KEY)
     return newsapi
 
 def get_articles(newsapi:NewsApiClient ,subject:str):
-    all_articles = newsapi.get_everything(q=subject)
+    all_articles = newsapi.get_everything(q=subject, sort_by='relevancy')
     return all_articles
 
 def convert_json(data):
     json_obj = json.dumps(data, indent=4)
     return json_obj
 
-def get_status_code(data_json):
-
-    
+def parse_articles(data_json):
     nb_articles = data_json["totalResults"]
     articles = data_json["articles"]
     print(f"Number Articles found: {nb_articles}")
@@ -26,29 +28,55 @@ def get_status_code(data_json):
     lst_articles = data_json["articles"]
 
     print(f"Range: {len(lst_articles)}")
+    data = {}
+    data["Source"] = "NewsApi"
+
+    articles = []
 
     for article in lst_articles:
+        article_data = {}
+        article_data["Title"] = filter_strings(article["title"])
+        article_data["Description"] = filter_strings(article["description"])
+        article_data["Url"] = article["url"]
+        article_data["UrlImage"] = article["urlToImage"]
+        articles.append(article_data)
+    data["Post"] = articles
+    return data
+    
+def generate_json_file(file_name:str, data_dict:dict, directory:str):
+    out_file = open(f"{directory}{file_name}.json", "w")
+    json.dump(data_dict, out_file, indent=2)
+    out_file.close()
 
-        author = article["author"]
-        title = article["title"]
+def get_multiple_articles(newsapi):
+        subject_search = None
+        continue_scrapping = True
 
-        print(f"Titles: {title}")
+        while continue_scrapping:
+            while subject_search == None:
+                subject_search = input("Enter News name: ") or None
 
-def dump_json_file(data_json, file_name):
-   file1 = open(file_name, "w")
-   file1.write(str(data_json))
+            data_json_articles = get_articles(newsapi=newsapi,subject= subject_search)
+            new_data = parse_articles(data_json_articles)
+            generate_json_file(f"Data-{subject_search}", new_data, f"{DIRECTORY_DATABASE}")
 
+            user_input = input("Continue scrapping? (Y/N): ").lower() == "y"
+            if user_input == None:
+                user_input = False
+            continue_scrapping = user_input
+            subject_search = None
 
 def main():
     newsapi = init_newsapi()
 
     subject_search = "Good News"
     data_json_articles = get_articles(newsapi=newsapi,subject= subject_search)
-    data_json_string = convert_json(data_json_articles)
-    dump_json_file(data_json_string, f"Data-{subject_search}.json")
-    get_status_code(data_json_articles)
+    #data_json_string = convert_json(data_json_articles)
+    new_data = parse_articles(data_json_articles)
+    generate_json_file(f"Data-{subject_search}", new_data, f"{DIRECTORY_DATABASE}")
 
-    # print(data_json)
 
 if __name__ == "__main__":
-    main()
+    #main()
+    newsapi = init_newsapi()
+    get_multiple_articles(newsapi)
