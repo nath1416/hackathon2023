d9fdd5dbc154f8fe14da49c27de427220ce65ceb
Antoine-Lafrance <antoine.lafrance@polymtl.ca>
2023-02-05 09:25:26 -0500

    mots interdits

--- AI/google_dataset/aimain.py
+++ AI/google_dataset/aimain.py
@@ -26,27 +26,33 @@ def makeExamples(df, tags_pos, tags_neg):
     df = df.sample(n=2500)
 
     df["isTherePositive"] = False
-    df["label"] = 0
-    last_pos = pandas.Series([True for i in range(len(df))])
+    df["label"] = "0"
     for tag in tags_pos:
         df["isTherePositive"] = df["isTherePositive"] | df[tag]
         for i, square in enumerate(df["isTherePositive"]):
-            if (square == True) and (df.iloc[i, -1] == 0):
+            if (square == True) and (df.iloc[i, -1] == "0"):
                 df.iloc[i, -1] = tag
 
     for tag in tags_neg:
         df["isTherePositive"] = df["isTherePositive"] & ~df[tag]
 
     examples = []
-
+    mot_inderdit = False
     for i, row in df.iterrows():
         for cancel in cancel_words:
             if cancel in row[1]:
-                examples.append(row[1], 0)
-            else:
-                examples.append(Example(row[1], str(row[-1])))
+                mot_inderdit = True
 
-    print(df)
+        if mot_inderdit:
+            examples.append(Example(row[1], "0"))
+            mot_inderdit = False
+
+        elif row[-2]:
+            examples.append(Example(row[1], str(row[-1])))
+
+        else:
+            examples.append(Example(row[1], "0"))
+
     return examples
 
 
@@ -56,17 +62,21 @@ examples = makeExamples(df, positive, negative)
 def isAtricleGoodFromComments(comments: List[str]) -> bool:
     # temporary
     input = [",".join(comments)]
-    print(type(input))
-
+    banned_words = ["drown", "kill", "rape", "murder", "crime"]
+    for banned_word in banned_words:
+        if banned_word in input[0]:
+            print(f"mot inderdit {banned_word}")
+            return "0", 0
     pred = co.classify(
         examples=examples,
         inputs=input,
         model="large",
     ).classifications[0]
-    return "" if pred.prediction == 0 else pred.prediction, pred.confidence
+    return "" if pred.prediction == "0" else pred.prediction, pred.confidence
 
 
-print(examples[5])
 print(
-    isAtricleGoodFromComments(["hello, i absolutely love and adore this post wow!!!"])
+    isAtricleGoodFromComments(
+        ["hello, i absolutely love and adore this post wow killer!!!"]
+    )
 )
